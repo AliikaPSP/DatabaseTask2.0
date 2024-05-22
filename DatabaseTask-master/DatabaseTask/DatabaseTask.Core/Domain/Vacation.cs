7222f0e0b92ce159a4b088d027b35b8c61a6ce23
AliikaPSP <aliika.puusepp@hotmail.com>
2024-05-22 17:02:53 +0300

    Add classes

---
+++ DatabaseTask-master/DatabaseTask/DatabaseTask.Core/Domain/Vacation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatabaseTask.Core.Domain
+{
+    public class Vacation
+    {
+        [Key]
+        public Guid VacationId { get; set; }
+
+        // Foreign key
+        [ForeignKey("Employee")]
+        public Guid EmployeeId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Type { get; set; }
+
+        // Navigation property
+        public Employee Employee { get; set; }
+    }
+}
